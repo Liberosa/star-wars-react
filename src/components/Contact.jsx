dc29f79691458543d9d2546ffe666e1dfcf8dbb3
Dmitry <smaragdus46@gmail.com>
2025-07-09 19:27:47 +0300

    storage

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,34 +3,38 @@ import {baseUrl} from "../utils/constants.js";
 
 export const Contact = () => {
     const [planetData, setPlanetData] = useState([]);
+
+    async function fetchPlanetData() {
+        const res = await fetch(`${baseUrl}/v1/planets`)
+        const data = await res.json();
+        setPlanetData(data)
+
+    }
+
     useEffect(() => {
-        fetch(`${baseUrl}/v1/planets`)
-            .then(res => res.json())
-            .then(data => {
-                setPlanetData(data)
-            })
+        fetchPlanetData();
     }, []);
     return (<div className="container">
-            <form>
+        <form>
 
-                <label htmlFor="fname">First Name</label>
-                <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
+            <label htmlFor="fname">First Name</label>
+            <input type="text" id="fname" name="firstname" placeholder="Your name.."/>
 
-                <label htmlFor="lname">Last Name</label>
-                <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
+            <label htmlFor="lname">Last Name</label>
+            <input type="text" id="lname" name="lastname" placeholder="Your last name.."/>
 
-                <label htmlFor="planet">Planet</label>
-                <select id="planet" name="planet">
+            <label htmlFor="planet">Planet</label>
+            <select id="planet" name="planet">
 
-                    {planetData.map(item => <option key={item.id} value={item.name}>{item.name}</option>)}
+                {planetData.map(item => <option key={item.id} value={item.name}>{item.name}</option>)}
 
-                </select>
+            </select>
 
-                <label htmlFor="subject">Subject</label>
-                <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
+            <label htmlFor="subject">Subject</label>
+            <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
 
-                <input type="submit" value="Submit"/>
+            <input type="submit" value="Submit"/>
 
-            </form>
-        </div>)
+        </form>
+    </div>)
 }
--- src/components/OpeningCrawl.jsx
+++ src/components/OpeningCrawl.jsx
@@ -4,10 +4,21 @@ import {baseUrl} from "../utils/constants.js";
 const OpeningCrawl = () => {
     const [openingCrawl, setOpeningCrawl] = useState('');
     useEffect(() => {
-        const episode = Math.floor(Math.random() * 6) + 1;
-        fetch(`${baseUrl}/v1/films/${episode}`)
-            .then(res => res.json())
-            .then(data => setOpeningCrawl(data.opening_crawl));
+        const opening_crawl = sessionStorage.getItem("opening_crawl");
+        if (opening_crawl) {
+            setOpeningCrawl(opening_crawl);
+        } else {
+            const episode = Math.floor(Math.random() * 6) + 1;
+            fetch(`${baseUrl}/v1/films/${episode}`)
+                .then(res => res.json())
+                .then(data => {
+                        setOpeningCrawl(data.opening_crawl)
+                        sessionStorage.setItem("opening-crawl", data.opening_crawl);
+                    }
+                );
+        }
+
+
     }, [])
     return (
         <div className="farGalaxy">{openingCrawl ? openingCrawl :
